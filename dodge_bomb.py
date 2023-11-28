62a3258fd9d1eaec0d7ef091a3ee5c8bce1466c2
C0B22123 <C0B22123d2@edu.teu.ac.jp>
2023-11-28 14:11:47 +0900

    爆弾追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -11,6 +11,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb = pg.Surface((20, 20))  # 練習1:透明のSurfaceを作る
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)  # 練習1:半径10の円を生成
+    bomb_rct = bomb.get_rect()  #練習2:爆弾を
+    bomb_rct.centerx = random.randint(0, WIDTH)
+    bomb_rct.centery = random.randint(0, HEIGHT)
+    bomb.set_colorkey((0, 0, 0))
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +27,8 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb, bomb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(10)
